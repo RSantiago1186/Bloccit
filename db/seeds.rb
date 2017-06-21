dafa6b338f7a967ecd9a97fbe0f67bfb30f3daf0
Ray Santiago <rsantiago1186@gmail.com>
2017-06-21 05:28:56 +0000

    Seeding Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Posts
+50.times do
+  Post.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+    )
+end
+posts = Post.all
+
+# Create Comments
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+    )
+end
+
+# Create a custom post and check to make sure it doesn't already exist
+post = [
+  { title: "Test Title", body: "Sample body test" }
+]
+
+post.each do |post|
+  Post.where(post).first_or_create
+end
+
+# Create a custom comment and check to make sure it doesn't already exist
+comment = [
+  { post: "This is a test post", body: "Sample test for the body of the comment" }
+]
+
+comment.each do |comment|
+  Comment.where(comment).first_or_create
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
